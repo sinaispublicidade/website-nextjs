0b3673a20f419a6d498b826ef69359c4ba4062da
Leônidas Santos <oi@ltco.com.br>
2025-09-19 18:32:49 -0300

    feat: add form schemas for About, Ads, and Home

---
+++ src/schemas/forms/ads.schema.ts
@@ -0,0 +1,21 @@
+import z from 'zod'
+import { validatePhone } from 'validations-br'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { validationMessages } from '@/lib/validation-messages'
+
+export const AdsFormSchema = z.object({
+  nome: z.string().min(2, validationMessages.required.default),
+  email: z.email(validationMessages.invalid.email),
+  telefone: z
+    .string()
+    .min(8, validationMessages.required.phone)
+    .refine((val) => validatePhone(val), {
+      message: validationMessages.invalid.phone,
+    }),
+  faturamento: z.string().min(2, validationMessages.required.default),
+  tipoServico: z.string().min(2, validationMessages.required.default),
+  tiporRetorno: z.string(),
+})
+
+export const AdsFormResolver = zodResolver(AdsFormSchema)
