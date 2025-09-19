8f22d8a1b671cd2935956f3e6816d26b5d6b7c8d
Leônidas Santos <oi@ltco.com.br>
2025-09-19 18:36:56 -0300

    feat: add migration files and Prisma schema

---
+++ prisma/migrations/20250919122300_create_table_ads_form/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `form_ads` (
+    `id` VARCHAR(191) NOT NULL,
+    `nome` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `telefone` VARCHAR(191) NOT NULL,
+    `faturamento` VARCHAR(191) NOT NULL,
+    `tipo_servico` VARCHAR(191) NOT NULL,
+    `tipo_retorno` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
