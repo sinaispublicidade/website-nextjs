5e184ea596abce5122d1766b749342ef2a4888d4
Leônidas Santos <oi@ltco.com.br>
2025-09-19 18:30:29 -0300

    feat: add validation messages for form inputs

---
+++ src/lib/validation-messages.ts
@@ -0,0 +1,19 @@
+export const validationMessages = {
+  required: {
+    default: 'Campo obrigatório.',
+    phone: 'Telefone obrigatório.',
+    number: 'Número obrigatório.',
+    email: 'E-mail obrigatório.',
+    date: 'Data obrigatória.',
+  },
+  invalid: {
+    default: 'Campo inválido.',
+    number: 'Número inválido.',
+    phone: 'Telefone inválido.',
+    email: 'E-mail inválido.',
+    date: 'Data inválida.',
+  },
+  minLength: (min: number) => `O campo deve ter no mínimo ${min} caracteres.`,
+  minArray: 'Selecione ao menos 1 (um) valor.',
+  terms: 'Assinale a caixa para continuarmos.',
+}
