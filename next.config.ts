24942cc231f1c81f29759f25255698fff785957b
Leônidas Santos <oi@ltco.com.br>
2025-09-20 17:02:51 -0300

    feat: update next.config.ts to configure ESLint settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  eslint: {
+    ignoreDuringBuilds: true, // This will ignore ESLint errors during the build process
+  },
+}
 
-export default nextConfig;
+export default nextConfig
