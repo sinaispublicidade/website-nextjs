a5210d0aa567450026b0a6b50a1955d729575d59
Leônidas Santos <oi@ltco.com.br>
2025-09-20 13:09:07 -0300

    feat: add BannerHeroAbout component

---
+++ src/components/Banners/About.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Image from 'next/image'
+
+import { Button } from '@/components/ui/Button'
+
+export const BannerHeroAbout: React.FC = () => {
+  return (
+    <>
+      <Image
+        src="/assets/banners/banner-hero--about.png"
+        alt="Banner Hero"
+        width={1920}
+        height={1080}
+        className="user-select-none pointer-events-none absolute inset-0 h-full w-full object-cover"
+        priority
+      />
+      <div className="relative z-10 mx-auto flex max-w-7xl flex-col items-center justify-between gap-4 px-8 sm:grid sm:grid-cols-5 md:px-12">
+        <div className="w-full max-w-md font-sans sm:col-span-3 sm:max-w-lg sm:pb-8 lg:max-w-2xl">
+          <h1 className="text-2xl font-bold sm:text-3xl lg:text-[40px]">
+            Há mais de 15 anos gerando resultados reais
+          </h1>
+
+          <blockquote className="mt-4">
+            <p className="mb-8 text-base italic md:text-lg lg:text-xl">
+              Há mais de 15 anos, venho ajudando empresas a transformarem
+              desafios em oportunidades. Na SINAIS, criamos soluções que
+              aproximam marcas de pessoas e geram resultados reais que
+              potencializam negócios.
+            </p>
+            <p className="text-xl">
+              <strong className="font-semibold">Primo Junior</strong> -
+              Co-Founder SINAIS
+            </p>
+          </blockquote>
+
+          <Button.Outline
+            href="/orcamento"
+            className="bg-background/60 relative mt-8"
+            plusIcon
+          >
+            Pedir orçamento
+          </Button.Outline>
+        </div>
+
+        <div className="relative col-span-2">
+          <Image
+            src="/assets/young_executive_man.png"
+            alt="Young Executive"
+            width={600}
+            height={600}
+            className="pointer-events-none object-contain select-none"
+            priority
+          />
+        </div>
+      </div>
+    </>
+  )
+}
