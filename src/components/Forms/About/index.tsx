ede560b8d16b9018a10f595fcd5285baf16abe2a
Leônidas Santos <oi@ltco.com.br>
2025-09-20 13:35:42 -0300

    feat: add AboutForm component

---
+++ src/components/Forms/About/index.tsx
@@ -0,0 +1,94 @@
+'use client'
+import React, { useCallback, useState } from 'react'
+import { useForm } from 'react-hook-form'
+
+import { AboutFormData, SubmitStatus } from '@/types'
+
+import { AboutFormResolver } from '@/schemas/forms/about.schema'
+import { handleAboutForm } from '@/app/actions'
+
+import { CustomAlert } from '@/components/CustomAlert'
+import { Input } from '@/components/ui/Input'
+import { Button } from '@/components/ui/Button'
+
+export const AboutForm: React.FC = () => {
+  const [submitStatus, setSubmitStatus] = useState<SubmitStatus>(null)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<AboutFormData>({
+    resolver: AboutFormResolver,
+  })
+
+  const onSubmit = useCallback(
+    async (data: AboutFormData) => {
+      setSubmitStatus(null)
+
+      try {
+        await handleAboutForm(data)
+        setSubmitStatus('success')
+        reset()
+      } catch (error) {
+        setSubmitStatus('error')
+        console.error(error)
+      }
+    },
+    [reset, setSubmitStatus, handleAboutForm]
+  )
+
+  return (
+    <form
+      className="bg-sinais-darkblue flex w-full flex-col gap-4 p-6 sm:rounded-xl lg:p-10"
+      onSubmit={handleSubmit(onSubmit)}
+      noValidate
+    >
+      <Input
+        placeholder="Nome*"
+        registration={register('nome')}
+        error={errors.nome}
+      />
+      <Input
+        placeholder="Email*"
+        registration={register('email')}
+        error={errors.email}
+        type="email"
+      />
+      <Input
+        placeholder="Telefone*"
+        registration={register('telefone')}
+        error={errors.telefone}
+      />
+      <Input
+        placeholder="Site atual*"
+        registration={register('siteAtual')}
+        error={errors.siteAtual}
+      />
+      <Input
+        placeholder="Tipo de serviço?*"
+        registration={register('tipoServico')}
+        error={errors.tipoServico}
+      />
+      <Input
+        placeholder="Como prefere o retorno?"
+        registration={register('tiporRetorno')}
+        error={errors.tiporRetorno}
+      />
+
+      <div className="text-center sm:mt-4">
+        <Button.Submit
+          type="submit"
+          className="w-full sm:w-auto"
+          isSending={isSubmitting}
+          disabled={isSubmitting}
+          invert
+        >
+          Solicitar contato
+        </Button.Submit>
+      </div>
+
+      {submitStatus && <CustomAlert type={submitStatus} className="w-full" />}
+    </form>
+  )
+}
