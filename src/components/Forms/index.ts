b8446fafbf308847994e768f3cec89c641f90e99
Leônidas Santos <oi@ltco.com.br>
2025-09-20 13:36:06 -0300

    feat: add Forms index file to export form components

---
+++ src/components/Forms/index.ts
@@ -0,0 +1,9 @@
+import { AboutForm } from './About'
+import { AdsForm } from './Ads'
+import { HomeForm } from './Home'
+
+export const Forms = {
+  About: AboutForm,
+  Ads: AdsForm,
+  Home: HomeForm,
+}
