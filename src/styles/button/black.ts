9d9a2051274d3ca5105269b10ba9b16b0e7b25dd
Leônidas Santos <oi@ltco.com.br>
2025-09-22 19:51:32 -0300

    feat: add button style modules

---
+++ src/styles/button/black.ts
@@ -0,0 +1,6 @@
+export const BLACK_BUTTON_STYLES = [
+  'bg-black',
+  'text-white',
+  'hover:bg-white',
+  'hover:text-black',
+].join(' ')
---
+++ src/styles/button/blue.ts
@@ -0,0 +1,9 @@
+export const BLUE_BUTTON_STYLES = [
+  'bg-sinais-blue',
+  'border-sinais-blue',
+  'hover:text-white',
+  'hover:border-sinais-blue',
+  'hover:bg-sinais-blue/80',
+  'animate-(--pulse-shadow-sinais-blue)',
+  'mx-auto',
+].join(' ')
