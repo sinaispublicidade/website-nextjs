fdb5a8bd3214705159ecc4f3cfd282d77822f981
Leônidas Santos <oi@ltco.com.br>
2025-09-25 16:01:59 -0300

    feat: add BannerHeroServiceImage component and integrate into Banner

--- src/components/Banners/index.tsx
+++ src/components/Banners/index.tsx
@@ -2,12 +2,16 @@ import { BannerHeroHome } from './Home'
 import { BannerHeroAbout } from './About'
 import { BannerHeroService } from './Service'
 import { BannerHeroServiceCTA } from './ServiceCTA'
+import { BannerHeroServiceImage } from './ServiceImage'
 
 export const Banner = {
   Home: BannerHeroHome,
   About: BannerHeroAbout,
   Service: {
     Default: BannerHeroService,
-    CTA: BannerHeroServiceCTA,
+    CTA: {
+      Background: BannerHeroServiceCTA,
+      Image: BannerHeroServiceImage,
+    },
   },
 }
