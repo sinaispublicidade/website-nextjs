767f21b5990db0900ffc5281b8ce9e58f53bc745
Leônidas Santos <oi@ltco.com.br>
2025-09-25 16:00:57 -0300

    refactor: update layout and styling in BannerHeroAbout component

--- src/components/Banners/About.tsx
+++ src/components/Banners/About.tsx
@@ -14,14 +14,14 @@ export const BannerHeroAbout: React.FC = () => {
         className="user-select-none pointer-events-none absolute inset-0 h-full w-full object-cover"
         priority
       />
-      <div className="relative z-10 mx-auto flex max-w-7xl flex-col items-center justify-between gap-4 px-8 sm:grid sm:grid-cols-5 md:px-12">
-        <div className="w-full max-w-md font-sans sm:col-span-3 sm:max-w-lg sm:pb-8 lg:max-w-2xl">
-          <h1 className="text-2xl font-bold sm:text-3xl lg:text-[40px]">
+      <div className="relative z-10 mx-auto flex max-w-7xl flex-col items-center justify-between gap-4 px-8 md:grid md:grid-cols-10 md:items-end md:px-12">
+        <div className="w-full max-w-md font-sans sm:max-w-lg sm:pb-8 md:col-span-5 lg:col-span-6 lg:max-w-2xl">
+          <h1 className="text-2xl font-bold sm:text-3xl xl:text-[40px]">
             Há mais de 15 anos gerando resultados reais
           </h1>
 
           <blockquote className="mt-4">
-            <p className="mb-8 text-base italic md:text-lg lg:text-xl">
+            <p className="mb-8 text-base italic md:text-lg xl:text-xl">
               Há mais de 15 anos, venho ajudando empresas a transformarem
               desafios em oportunidades. Na SINAIS, criamos soluções que
               aproximam marcas de pessoas e geram resultados reais que
@@ -42,13 +42,13 @@ export const BannerHeroAbout: React.FC = () => {
           </Button.Outline>
         </div>
 
-        <div className="relative col-span-2">
+        <div className="relative md:col-span-5 lg:col-span-4">
           <Image
             src="/assets/young_executive_man.png"
             alt="Young Executive"
             width={600}
             height={600}
-            className="pointer-events-none object-contain select-none"
+            className="pointer-events-none object-contain select-none sm:max-w-[400px] lg:max-w-full"
             priority
           />
         </div>
