0da5d93d2a4a7a294d92d4108f2681fb128e3b41
Leônidas Santos <oi@ltco.com.br>
2025-09-29 15:57:06 -0300

    fix: update Button.Outline class to 'stretched-link' in Portfolio component

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -17,19 +17,19 @@ export const Portfolio: React.FC<PortfolioProps> = ({
 }) => (
   <div className={cn('grid sm:grid-cols-2 lg:grid-cols-3', className)}>
     {portfolio.slice(0, quantity).map(({ name, link, image }, idx) => (
-      <div key={idx} className="relative h-72 w-full">
+      <div key={idx} className="relative w-full md:h-72">
         <Image
           src={image}
           alt="Banner aleatório"
           width={960}
           height={540}
-          className="absolute inset-0 size-full object-cover"
+          className="object-cover md:absolute md:inset-0 md:size-full"
         />
 
-        <div className="bg-sinais-blue/80 absolute inset-0 flex size-full flex-col items-center justify-center p-5 opacity-0 transition-opacity hover:opacity-100">
+        <div className="bg-sinais-blue/80 absolute inset-0 flex size-full flex-col items-center justify-center gap-3 p-5 opacity-0 transition-opacity md:hover:opacity-100">
           <p className="text-lg font-medium lg:text-2xl">{name}</p>
 
-          <Button.Outline className="mt-3" href={link} plusIcon>
+          <Button.Outline className="stretched-link" href={link} plusIcon>
             Quero saber mais
           </Button.Outline>
         </div>
