8fe689e37689f56db7ad72a23156ccce6060fc7c
Leônidas Santos <oi@ltco.com.br>
2026-01-15 20:03:47 -0300

    feat: expand portfolio mock data with additional projects and categories

--- src/mocks/portfolio.mock.ts
+++ src/mocks/portfolio.mock.ts
@@ -1,53 +1,167 @@
-export interface IPortfolio {
-  name: string
-  image: string
-  link: string
-}
+import { IPortfolio } from '@/types'
 
-export const portfolio: IPortfolio[] = [
+export const portfolioHome: IPortfolio[] = [
   {
-    name: 'Projeto 1',
-    image: '/assets/portfolio/9978A_Portfólio_Sinais_2_2.png',
-    link: '#',
+    name: 'Anuncio de Revista',
+    image: '/assets/portfolio/home/Anuncio-de-Revista.jpg',
+    link: '/servicos/marketing-para-varejo',
   },
   {
-    name: 'Projeto 2',
-    image: '/assets/portfolio/9978A_Portfólio_Sinais_2_3.png',
-    link: '#',
+    name: 'Catalogo B2B',
+    image: '/assets/portfolio/home/Catalogo-B2B.jpg',
+    link: '/servicos/marketing-para-varejo',
   },
   {
-    name: 'Projeto 3',
-    image: '/assets/portfolio/9978A_Portfólio_Sinais_2_4.png',
-    link: '#',
+    name: 'Comunicacao EVOLASER',
+    image: '/assets/portfolio/home/Comunicacao-EVOLASER.jpg',
+    link: '/servicos/marketing-para-varejo',
   },
   {
-    name: 'Projeto 4',
-    image: '/assets/portfolio/9978A_Portfólio_Sinais_2_5.png',
-    link: '#',
+    name: 'Identidade de Marca',
+    image: '/assets/portfolio/home/Identidade-de-Marca.jpg',
+    link: '/servicos/design-de-marcas',
   },
   {
-    name: 'Projeto 5',
-    image: '/assets/portfolio/9978A_Portfólio_Sinais_2_7.png',
-    link: '#',
+    name: 'Material didatico',
+    image: '/assets/portfolio/home/Material-didatico.jpg',
+    link: '/servicos/design-de-marcas',
   },
   {
-    name: 'Projeto 6',
-    image: '/assets/portfolio/9978A_Portfólio_Sinais_2_9.png',
-    link: '#',
+    name: 'Padrao de marca',
+    image: '/assets/portfolio/home/Padrao-de-marca.jpg',
+    link: '/servicos/design-de-marcas',
   },
   {
-    name: 'Projeto 7',
-    image: '/assets/portfolio/9978A_Portfólio_Sinais_2_10.png',
-    link: '#',
+    name: 'Campanha de captacao',
+    image: '/assets/portfolio/home/Campanha-de-captacao.jpg',
+    link: '/servicos/marketing-para-varejo',
   },
   {
-    name: 'Projeto 8',
-    image: '/assets/portfolio/9978A_Portfólio_Sinais_2_11.png',
-    link: '#',
+    name: 'Comunicacao B2B',
+    image: '/assets/portfolio/home/Comunicacao-B2B.png',
+    link: '/servicos/marketing-para-varejo',
   },
   {
-    name: 'Projeto 9',
-    image: '/assets/portfolio/9978A_Portfólio_Sinais_2_13.png',
-    link: '#',
+    name: 'Design de embalagens',
+    image: '/assets/portfolio/home/Design-de-embalagens.jpg',
+    link: '/servicos/design-de-embalagens',
+  },
+  {
+    name: 'Lancamento Imobiliario',
+    image: '/assets/portfolio/home/Lancamento-Imobiliario.jpg',
+    link: '/servicos/inbound-rd-station',
+  },
+  {
+    name: 'Padrao de Embalagens',
+    image: '/assets/portfolio/home/Padrao-de-Embalagens.jpg',
+    link: '/servicos/design-de-embalagens',
+  },
+  {
+    name: 'Social Media',
+    image: '/assets/portfolio/home/Social-Media.jpg',
+    link: '/servicos/redes-sociais',
+  },
+]
+
+export const portfolioDesenvolvimentoSites: IPortfolio[] = [
+  {
+    name: 'Ambientaly',
+    image: '/assets/portfolio/desenvolvimento-de-sites/Ambientaly.png',
+    link: 'http://ambientaly.com',
+  },
+  {
+    name: 'Bauminas',
+    image: '/assets/portfolio/desenvolvimento-de-sites/Bauminas.png',
+    link: 'https://bauminas.com.br',
+  },
+  {
+    name: 'English House',
+    image: '/assets/portfolio/desenvolvimento-de-sites/English-House.jpg',
+    link: 'https://englishhousebz.com.br',
+  },
+]
+
+export const portfolioDesignEmbalagens: IPortfolio[] = [
+  {
+    name: 'Identidade e embalagens Bob Esponja',
+    image:
+      '/assets/portfolio/design-de-embalagens/Identidade-e-embalagens-Bob-Esponja.jpg',
+  },
+  {
+    name: 'Novas embalagens FEROZ',
+    image: '/assets/portfolio/design-de-embalagens/Novas-embalagens-FEROZ.jpg',
+  },
+  {
+    name: 'Padrão de embalagens ADERE',
+    image:
+      '/assets/portfolio/design-de-embalagens/Padrao-de-embalagens-ADERE.png',
+  },
+  {
+    name: 'Padrão de embalagens BAUMINAS',
+    image:
+      '/assets/portfolio/design-de-embalagens/Padrao-de-embalagens-BAUMINAS.jpg',
+  },
+  {
+    name: 'Padrão de embalagens BRAZZO',
+    image:
+      '/assets/portfolio/design-de-embalagens/Padrao-de-embalagens-BRAZZO.jpg',
+  },
+  {
+    name: 'Suplemento Nitromuscle',
+    image: '/assets/portfolio/design-de-embalagens/Suplemento-Nitromuscle.jpg',
+  },
+]
+
+export const portfolioDesignMarcas: IPortfolio[] = [
+  {
+    name: 'Construção de marca BRAZZO',
+    image: '/assets/portfolio/design-de-marcas/Construcao-de-marca-BRAZZO.jpg',
+  },
+  {
+    name: 'Construção de marca EVOLASER',
+    image:
+      '/assets/portfolio/design-de-marcas/Construcao-de-marca-EVOLASER.jpg',
+  },
+  {
+    name: 'Construção de marca Nonna Angellina',
+    image:
+      '/assets/portfolio/design-de-marcas/Construcao-de-marca-Nonna-Angellina.jpg',
+  },
+  {
+    name: 'Nova Marca Ateliê',
+    image: '/assets/portfolio/design-de-marcas/Nova-Marca-Atelie.jpg',
+  },
+  {
+    name: 'Nova marca de programa',
+    image: '/assets/portfolio/design-de-marcas/Nova-marca-de-programa.jpg',
+  },
+  {
+    name: 'Rebrand MazaShop',
+    image: '/assets/portfolio/design-de-marcas/Rebrand-MazaShop.jpg',
+  },
+  {
+    name: 'Construção de marca BZ',
+    image: '/assets/portfolio/design-de-marcas/Construcao-de-marca-BZ.jpg',
+  },
+  {
+    name: 'Construção de marca MIRA',
+    image: '/assets/portfolio/design-de-marcas/Construcao-de-marca-MIRA.jpg',
+  },
+  {
+    name: 'Contrução de marca PANOBIANCO',
+    image:
+      '/assets/portfolio/design-de-marcas/Contrucao-de-marca-PANOBIANCO.png',
+  },
+  {
+    name: 'Nova marca Grupo Pivot',
+    image: '/assets/portfolio/design-de-marcas/Nova-marca-Grupo-Pivot.jpg',
+  },
+  {
+    name: 'Rebrand Ayuso',
+    image: '/assets/portfolio/design-de-marcas/Rebrand-Ayuso.jpg',
+  },
+  {
+    name: 'Rebrand Teknoled',
+    image: '/assets/portfolio/design-de-marcas/Rebrand-Teknoled.jpg',
   },
 ]
