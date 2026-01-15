e649a7791fcc185bdfef3c507628c555f79ec5cd
Leônidas Santos <oi@ltco.com.br>
2026-01-15 19:58:54 -0300

    feat: add Select component type definitions and update related imports

--- src/types/react.d.ts
+++ src/types/react.d.ts
@@ -2,6 +2,7 @@ export declare namespace ReactProps {
   type Anchor = React.AnchorHTMLAttributes<HTMLAnchorElement>
   type Button = React.ButtonHTMLAttributes<HTMLButtonElement>
   type Input = React.InputHTMLAttributes<HTMLInputElement>
+  type Select = React.SelectHTMLAttributes<HTMLSelectElement>
   type Textarea = React.TextareaHTMLAttributes<HTMLTextAreaElement>
   type Div = React.HTMLAttributes<HTMLDivElement>
 }
