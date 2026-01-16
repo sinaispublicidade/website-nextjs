c0dcec172c10917d43089e72d86bedebb526c56d
Leônidas Santos <oi@ltco.com.br>
2026-01-16 15:41:30 -0300

    feat: add Paula Rodrigues testimonial

--- src/mocks/testimonials.mock.ts
+++ src/mocks/testimonials.mock.ts
@@ -12,13 +12,13 @@ export const testimonials: ITestimonial[] = [
     role: 'Co-founder Panobianco Academia',
     text: 'A SINAIS teve um papel essencial na construção da nossa identidade como academia. Foi responsável pela concepção da marca e da identidade visual, trazendo agilidade, experiência e uma visão estratégica que fizeram toda a diferença no posicionamento do negócio.',
   },
-  /* {
+  {
     image: '/assets/testimonials/testimonial-paula-rodrigues.jpg',
     author: 'Paula Rodrigues',
     role: 'Coordenadora de Marketing - BAUMINAS',
-    text: 'A SINAIS é uma parceira estratégica da BAUMINAS há muitos anos. Atende todo o nosso grupo com soluções completas de comunicação, sempre com visão de marca, consistência e profundidade estratégica. Ter um parceiro como a SINAIS, liderado pelo Knorral, é essencial para sustentar e evoluir nossas marcas.',
+    text: 'A SINAIS é uma parceira estratégica da BAUMINAS há muitos anos. Atende todo o nosso grupo com soluções completas de comunicação, sempre com visão de marca, consistência e profundidade estratégica. Ter um parceiro como a SINAIS, com este know-how, é essencial para sustentar e evoluir nossas marcas.',
   },
-  {
+  /* {
     image: '/assets/testimonials/testimonial-julian-bruzon.jpg',
     author: 'Julian Bruzon',
     role: 'CEO - OLIMPO S.A.',
